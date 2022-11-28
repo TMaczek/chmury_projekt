0335ff3dd17ec6e17c9d481fa2585b7c41824572
TMaczek <tomek.maczo@onet.pl>
2022-11-28 09:34:16 +0100

    env's added

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 from DatabaseApp import *
 from flask import Flask, render_template
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 
 
 def get_episodes_for_tests():
-    uri = "neo4j+s://1b0bc938.databases.neo4j.io"
-    user = "neo4j"
-    password = "cfG0JN4KRSAc1VjsB-xVyp7KO8jRsMvd7WNdGf0KMj4"
+    uri = os.environ.get("NEO4J_URI")
+    print(uri)
+    print("xd")
+    user = os.environ.get("NEO4J_USERNAME")
+    password = os.environ.get("NEO4J_PASSWORD")
+    # uri = "neo4j+s://1b0bc938.databases.neo4j.io"
+    # user = "neo4j"
+    # password = "cfG0JN4KRSAc1VjsB-xVyp7KO8jRsMvd7WNdGf0KMj4"
     db_app = DatabaseApp(uri, user, password)
 
     data = db_app.find_characters_episodes("Peridot")
 
     db_app.close()
     return data
+    #return "ohio"
 
 
 @app.route("/")
